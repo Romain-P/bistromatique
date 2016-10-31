15edc9a80df4f90afc69dc08b3ef5feae156a3b1
Romain <romain.pillot@epitech.eu>
2016-10-31 18:01:04 +0000

    adding base files

--- include/utils.h
+++ include/utils.h
@@ -5,14 +5,17 @@
 ** Login   <romain.pillot@epitech.net>
 ** 
 ** Started on  Fri Oct 28 01:28:02 2016 romain pillot
-** Last update Fri Oct 28 01:52:04 2016 romain pillot
+** Last update Mon Oct 31 16:07:22 2016 romain pillot
 */
 
 #ifndef UTILS_
 # define UTILS_
 
+# include "number.h"
+
 void	my_putchar(char c);
 void	my_putstr(char *str);
 int	my_strlen(char *str);
+char	is_greater(t_number a, t_number b);
 
-#endif /** UTILS_ **/
+#endif /** !UTILS_ **/
